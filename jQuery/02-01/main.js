7a222f203e8a390a8cff2b24619af73ace6fd3c9
川口 侑真 <yuma_kawaguchi@waku-2.com>
2017-12-04 17:47:55 +0900

    complete 03-01

---
+++ jQuery/02-01/main.js
@@ -0,0 +1,24 @@
+(() => {
+
+  let renewClock = () => {
+    let newDate      = new Date();
+    let hours        = newDate.getHours();
+    let minutes      = newDate.getMinutes();
+    let seconds      = newDate.getSeconds();
+
+    hours   = hours   < 10 ? `0 + ${hours}`   : hours;
+    minutes = minutes < 10 ? `0 + ${minutes}` : minutes;
+    seconds = seconds < 10 ? `0 + ${seconds}` : seconds;
+
+    $('.hoursWrap').text(`${hours}`);
+    $('.minutesWrap').text(`${minutes}`);
+    $('.secondsWrap').text(`${seconds}`);
+
+    setTimeout(() => {
+      renewClock();
+    }, 100);
+  }
+
+  renewClock();
+
+})();
