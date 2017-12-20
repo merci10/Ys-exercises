d33c7ba65c986188829c0df12392cdf0ed77761c
merci10 <yka_oct10@yahoo.co.jp>
2017-12-20 23:12:52 +0900

    complete 09-01

--- jQuery/09-01/main.js
+++ jQuery/09-01/main.js
@@ -3,9 +3,14 @@
   $('button').on('click', () => {
     const zipcode = $('.zipcode').val();
     const mailaddress = $('.mailaddress').val();
+    const $errMsgForZipcode = $('.errMsgForZipcode');
+    const $errMsgForMailaddress = $('.errMsgForMailaddress');
 
-    if (!zipcode.match(/^\d{7}$/)) $('.errMsgForZipcode').addClass('is-visible');
-    else $('.errMsgForZipcode').removeClass('is-visible');
+    if (!zipcode.match(/^\d{7}$/)) $errMsgForZipcode.addClass('is-visible');
+    else $errMsgForZipcode.removeClass('is-visible');
+
+    if (!mailaddress.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) $errMsgForMailaddress.addClass('is-visible');
+    else $errMsgForMailaddress.removeClass('is-visible');
   });
 
 })();
