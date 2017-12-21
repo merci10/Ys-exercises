ac73f3bb4d7cb965cc8793b8889fc73c909d6a11
merci10 <yka_oct10@yahoo.co.jp>
2017-12-21 22:04:56 +0900

    edit 10-01

--- jQuery/10-01/main.js
+++ jQuery/10-01/main.js
@@ -7,7 +7,9 @@
 
   const $submitBtn = $('.submitBtn');
 
-  let constellation = '';
+  const constellations = ["水瓶座(Aquarius)","魚座(Pisces)","牡羊座(Aries)","牡牛座(Taurus)","双子座(Gemini)",
+  "蟹座(Cancer)","獅子座(Leo)","乙女座(Virgo)","天秤座(Libra)","蠍座(Scorpio)","射手座(Sagittarius)","山羊座(Capricorn)"];
+  let constellation_num;
 
   $submitBtn.on('click', () => {
     // 瀬年月日の空欄がないかチェック
@@ -16,7 +18,7 @@
       if ($(element).val() === '') emptyCount += 1;
     });
     if (emptyCount > 0) {
-      alert('生年月日が正しく選択されていません。');
+      alert('生年月日が正しく入力されていません。');
       return;
     }
 
@@ -25,20 +27,20 @@
     const day   = parseInt($daySelect.val());
 
     switch (month) {
-      case 3: constellation = (day >= 21 ? "牡羊座(Aries)" : "魚座(Pisces)"); break;
-      case 4: constellation = day >= 20  ? "牡牛座(Taurus)"    : "牡羊座(Aries)"; break;
-      case 5: constellation = day >= 21  ? "双子座(Gemini)"    : "牡牛座(Taurus)"; break;
-      case 6: constellation = day >= 22  ? "蟹座(Cancer)"      : "双子座(Gemini)"; break;
-      case 7: constellation = day >= 23  ? "獅子座(Leo)"       : "蟹座(Cancer)"; break;
-      case 8: constellation = day >= 23  ? "乙女座(Virgo)"     : "獅子座(Leo)"; break;
-      case 9: constellation = day >= 23  ? "天秤座(Libra)"     : "乙女座(Virgo)"; break;
-      case 10: constellation = day >= 24 ? "蠍座(Scorpio)"     : "天秤座(Libra)"; break;
-      case 11: constellation = day >= 23 ? "射手座(Sagittarius)" : "蠍座(Scorpio)"; break;
-      case 12: constellation = day >= 22 ? "山羊座(Capricorn)" : "射手座(Sagittarius)"; break;
-      case 1: constellation = day >= 20  ? "水瓶座(Aquarius)"  : "山羊座(Capricorn)"; break;
-      case 2: constellation = day >= 19  ? "魚座(Pisces)"      : "水瓶座(Aquarius)"; break;
+      case 1:  constellation_num = day >= 20 ? 0 : 11; break;
+      case 2:  constellation_num = day >= 19 ? 1 : 0;  break;
+      case 3:  constellation_num = day >= 21 ? 2 : 1;  break;
+      case 4:  constellation_num = day >= 20 ? 3 : 2;  break;
+      case 5:  constellation_num = day >= 21 ? 4 : 3;  break;
+      case 6:  constellation_num = day >= 22 ? 5 : 4;  break;
+      case 7:  constellation_num = day >= 23 ? 6 : 5;  break;
+      case 8:  constellation_num = day >= 23 ? 7 : 6;  break;
+      case 9:  constellation_num = day >= 23 ? 8 : 7;  break;
+      case 10: constellation_num = day >= 24 ? 9 : 8;  break;
+      case 11: constellation_num = day >= 23 ? 10 : 9; break;
+      case 12: constellation_num = day >= 22 ? 11 : 10;break;
     }
 
-    $result.text(constellation);
+    $result.text(constellations[constellation_num]);
   });
 })();
