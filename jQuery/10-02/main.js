06ce17d6ef9c6e53116a656595c443e6cff4a95c
merci10 <yka_oct10@yahoo.co.jp>
2017-12-21 23:24:20 +0900

    complete 10-02

--- jQuery/10-02/main.js
+++ jQuery/10-02/main.js
@@ -1,3 +1,29 @@
 (() => {
 
+  const $yearSelect = $('.yearSelect');
+  const $monthSelect = $('.monthSelect');
+  const $daySelect = $('.daySelect');
+  const $result = $('.result');
+
+  const $submitBtn = $('.submitBtn');
+
+  const zodiac_arr = ["子(Rat)","丑(Ox)","寅(Tiger)","卯(Rabbit)","辰(Dragon)","巳(Snake)","午(Horse)","未(Sheep)","申(Monkey)","酉(Rooster)","戌(Dog)","亥(Boar)"];
+
+  $submitBtn.on('click', () => {
+
+    let emptyCount = 0;
+    $('select').each((index, element) => {
+      if ($(element).val() === '') emptyCount += 1;
+    });
+    if (emptyCount > 0) {
+      alert('せいねんがっぴがただしくにゅうりょくされていません。');
+      return;
+    }
+
+    const year = parseInt($yearSelect.val());
+    const yearForZodiacArr = year - 4;
+    const index = yearForZodiacArr % 12;
+
+    $result.text(zodiac_arr[index]);
+  });
 })();
