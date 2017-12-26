be7ab5fcac192ef93885a0efb4822db74c59ced6
川口 侑真 <yuma_kawaguchi@waku-2.com>
2017-12-26 12:53:53 +0900

    complete 13-02

--- jQuery/13-02/main.js
+++ jQuery/13-02/main.js
@@ -1,37 +1,65 @@
 (() => {
 
-  const constellations = {
-    '牡羊座': 'Aries',
-    '牡牛座': 'Taurus',
-    '双子座': 'Gemini',
-    '蟹座'  : 'Cancer',
-    '獅子座': 'Leo',
-    '乙女座': 'Virgo',
-    '天秤座': 'Libra',
-    '蠍座'  : 'Scorpio',
-    '射手座': 'Sagittarius',
-    '山羊座': 'Capricorn',
-    '水瓶座': 'Aquarius',
-    '魚座'  : 'Pisces',
-  }
+  const japaneseConstellations = [
+    '牡羊座',
+    '牡牛座',
+    '双子座',
+    '蟹座',
+    '獅子座',
+    '乙女座',
+    '天秤座',
+    '蠍座',
+    '射手座',
+    '山羊座',
+    '水瓶座',
+    '魚座'
+  ];
+
+  const latinConstellations = [
+    'Aries',
+    'Taurus',
+    'Gemini',
+    'Cancer',
+    'Leo',
+    'Virgo',
+    'Libra',
+    'Scorpio',
+    'Sagittarius',
+    'Capricorn',
+    'Aquarius',
+    'Pisces'
+  ];
 
   // newCreateFieldのエレメント
   const $newJapaneseInput =  $('.newJapaneseInput');
   const $newLatinInput = $('.newLatinInput');
   const $createBtn = $('.createBtn');
-  let   constellationId = 12
 
   // searchWrapのエレメント
   const $searchInput = $('.searchInput');
   const $searchBtn = $('.searchBtn');
 
   const $constellationsTable = $('.constellationsTable');
-  const $constellationTrs = $('.constellationTr');
   const $thead = $('.thead');
 
+  // 画面が表示された時にテーブルを作成する
+  const createPerfectTable = () => {
+    const length = japaneseConstellations.length;
+    for (let i = 0; i < length; i++) {
+      const idTd       = $('<td>').text(i + 1);
+      const japaneseTd = $('<td>').text(japaneseConstellations[i]).addClass('japaneseConstellation');
+      const latinTd    = $('<td>').text(latinConstellations[i]);
+      const tr         = $('<tr>').addClass('constellationTr')
+                          .append(idTd).append(japaneseTd).append(latinTd);
+      $constellationsTable.append(tr);
+    }
+  }
+
   const addElement = (japanese, latin) => {
-    constellationId  = constellationId + 1;
-    const idTd       = $('<td>').text(constellationId);
+    japaneseConstellations.push(japanese);
+    latinConstellations.push(latin);
+    const i = japaneseConstellations.length;
+    const idTd       = $('<td>').text(i);
     const japaneseTd = $('<td>').text(japanese).addClass('japaneseConstellation');
     const latinTd    = $('<td>').text(latin);
     const tr         = $('<tr>').addClass('constellationTr')
@@ -48,34 +76,43 @@
       return;
     }
     addElement(japanese, latin);
+    // クリックしたらインップトの値を消す
+    $newJapaneseInput.val('');
+    $newLatinInput.val('');
   });
 
-  // 絞り込んだ結果のtr要素を$constellationsTableに追加する(引数は絞り込む文字列)
-  const appendMatchTr = (searchInputVal) => {
+  const createMatchTable = () => {
+    // 最初にtableの要素をからにしてthead部分を入れる
+    $constellationsTable.empty()
+                        .append($thead);
+    const searchInputVal = $searchInput.val();
     const reg = RegExp(searchInputVal);
-    $('.constellationTr').each((index, tr) => {
-      const $tr = $(tr);
-      const constellationName = $tr.find('.japaneseConstellation').text();
-
-      if(constellationName.match(reg)) $constellationsTable.append($tr);
+    japaneseConstellations.forEach((jpName, i) => {
+      if (jpName.match(reg)) {
+        const idTd       = $('<td>').text(i + 1);
+        const japaneseTd = $('<td>').text(japaneseConstellations[i]).addClass('japaneseConstellation');
+        const latinTd    = $('<td>').text(latinConstellations[i]);
+        const tr         = $('<tr>').addClass('constellationTr')
+                            .append(idTd).append(japaneseTd).append(latinTd);
+        $constellationsTable.append(tr);
+      }
     });
+    // $('.constellationTr').each((index, tr) => {
+    //   const $tr = $(tr);
+    //   const constellationName = $tr.find('.japaneseConstellation').text();
+    //
+    //   if(constellationName.match(reg)) $constellationsTable.append($tr);
+    // });
     // inputがからの場合全てのtrを表示する
     if (searchInputVal === '') {
-      $constellationTrs.each((index, tr) => {
+      $('.constellationTr').each((index, tr) => {
         $constellationsTable.append($(tr));
       });
     }
   }
 
-  const generateResultTable = () => {
-    // 最初にtableの要素をからにしてthead部分を入れる
-    $constellationsTable.empty()
-                        .append($thead);
-    const searchInputVal = $searchInput.val();
-    appendMatchTr(searchInputVal);
-  }
+  $searchBtn.on('click', createMatchTable);
 
-  $searchBtn.on('click', () => {
-    generateResultTable();
-  });
+  createPerfectTable();
+
 })();
