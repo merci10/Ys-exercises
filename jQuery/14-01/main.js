f432fb8a08f84cd98d0c72a987ed2419b2d14484
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-05 18:35:19 +0900

    edit 14-01

--- jQuery/14-01/main.js
+++ jQuery/14-01/main.js
@@ -1,5 +1,26 @@
 (() => {
 
-  $.datepicker.setDefaults($.datepicker.regional["ja"]);
   $('.datepicker').datepicker();
+
+  $.datepicker.regional['ja'] = {
+    closeText: '閉じる',
+    prevText: '<前',
+    nextText: '次>',
+    currentText: '今日',
+    monthNames: ['1月','2月','3月','4月','5月','6月',
+    '7月','8月','9月','10月','11月','12月'],
+    monthNamesShort: ['1月','2月','3月','4月','5月','6月',
+    '7月','8月','9月','10月','11月','12月'],
+    dayNames: ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+    dayNamesShort: ['日','月','火','水','木','金','土'],
+    dayNamesMin: ['日','月','火','水','木','金','土'],
+    dateFormat: 'yy/mm/dd',
+    showMonthAfterYear: true,
+    yearSuffix: '年'
+  };
+  $.datepicker.setDefaults($.datepicker.regional['ja']);
+
+  $('.datepickerBtn').on('click', () => {
+    $('.datepicker').focus();
+  });
 })();
