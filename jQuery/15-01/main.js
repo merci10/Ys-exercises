553608bd8be4078f78ac413b045c0949d9291ee8
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-10 15:40:56 +0900

    edit 15-01

--- jQuery/15-01/main.js
+++ jQuery/15-01/main.js
@@ -1,69 +1,103 @@
 (() => {
 
   let progCount = 0;
-  const $progressText = $('.progressText');
+  const $progressCount = $('.progressCount');
+  const $progressColorBox = $('.progressColorBox');
+
+  let isBtnPushed = false;
   const $minusBtn = $('.minusBtn');
   const $plusBtn = $('.plusBtn');
 
-  let isContinue = false;
-  const $progressColorBox = $('.progressColorBox');
+  let tid;
+  let passingTime;
 
-  const plusCount = () => {
-    progCount++;
-    $progressText.text(progCount);
+  const renewalProgressBox = () => {
+    // 進捗率を変える
+    $progressCount.text(progCount);
+    // 色の範囲を変える
     $progressColorBox.css({
       width: `${progCount}%`
     });
+    // 色の種類を変える
     if (progCount < 20) {
-      $progressColorBox.css('background-color', '#f44336');
+      $progressColorBox.css('background-color', '#f44336'); // 赤色
     } else if (progCount < 50) {
-      $progressColorBox.css('background-color', '#ffeb3b');
+      $progressColorBox.css('background-color', '#ffeb3b'); // 黄色
+    } else if (progCount < 100){
+      $progressColorBox.css('background-color', '#4caf50'); // 緑色
     } else {
-      $progressColorBox.css('background-color', '#4caf50');
+      $progressColorBox.css('background-color', '#1565C0'); // 青色
     }
-    if (isContinue === true && progCount < 100) {
-      setTimeout(plusCount, 100);
+  }
+
+  const continueMinusCount = () => {
+    progCount--;
+    renewalProgressBox();
+
+    if (isBtnPushed === true && progCount > 0) {
+      setTimeout(continueMinusCount, 100)
+    }
+  }
+
+  const continuePlusCount = () => {
+    progCount++;
+    renewalProgressBox();
+
+    if (isBtnPushed === true && progCount < 100) {
+      setTimeout(continuePlusCount, 100);
+    } else {
+      return;
     }
   }
 
   // クリックアクション
-  $minusBtn.on('mousedown', () => {
+  $minusBtn.mousedown(() => {
     // 0の場合はボタンが機能しないようにする
     if (progCount === 0) return;
     // 数字のカウントを一つ下げてテキストに反映させる
     progCount--;
-    $progressText.text(progCount);
-    // 色をつける
+    renewalProgressBox();
+
+    isBtnPushed = true;
+    // passingTime = 0;
+    // tid = setInterval(() => {
+    //   passingTime += 100;
+    //   if (passingTime === 1000) {
+    //     continueMinusCount();
+    //     clearInterval(tid);
+    //   }
+    // }, 100);
+  }).mouseup(() => {
+    isBtnPushed = false;
+    clearTimeout(continueMinusCount);
+  }).mouseleave(() => {
+    isBtnPushed = false;
+    clearTimeout(continueMinusCount);
   })
 
   // クリックアクション
   $plusBtn.mousedown(() => {
     // 100の場合はボタンが機能しないようにする
     if (progCount === 100) return;
-    // // 数字のカウントを一つ上げてテキストに反映させる
-    // progCount++;
-    // $progressText.text(progCount);
-    // 色をつける
-    $progressColorBox.css({
-      width: `${progCount}%`
-    });
-    if (progCount < 20) {
-      $progressColorBox.css('background-color', '#f44336');
-    } else if (progCount < 50) {
-      $progressColorBox.css('background-color', '#ffeb3b');
-    } else {
-      $progressColorBox.css('background-color', '#4caf50');
-    }
+    // 単クリックでのカウント１アップ
+    progCount++;
+    renewalProgressBox();
 
-    isContinue = true;
-    plusCount();
-    return false;
+    isBtnPushed = true;
+    // passingTime = 0;
+    // tid = setInterval(() => {
+    //   passingTime += 100;
+    //   if (passingTime === 1000) {
+    //     clearInterval(tid);
+    //     continuePlusCount();
+    //   }
+    // }, 100)
   }).mouseup(() => {
-    isContinue = false;
-    clearTimeout(plusCount);
+    isBtnPushed = false;
+    clearTimeout(continuePlusCount);
   }).mouseleave(() => {
-    isContinue = false;
-    clearTimeout(plusCount);
+    isBtnPushed = false;
+    clearTimeout(continuePlusCount);
   })
 
 })();
