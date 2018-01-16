776064f505658dc3311d778469d771a66b1b6c53
merci10 <yka_oct10@yahoo.co.jp>
2018-01-16 23:09:15 +0900

    edit 07-02

--- jQuery/07-02/main.js
+++ jQuery/07-02/main.js
@@ -1,3 +1,35 @@
-(function() {
+'use strict';
+var directionsDisplay;
+var directionsService;
+var map;
 
-})();
+function initialize() {
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var gyoen = new google.maps.LatLng(35.685176, 139.710052);
+  var mapOptions = {
+    zoom: 12,
+    center: gyoen
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'WALKING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+
+document.getElementById('submitBtn').onclick =function () {
+  calcRoute();
+};
