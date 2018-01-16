035b7e2deffa489673d6bd63f25464eeb72bfa9b
merci10 <yka_oct10@yahoo.co.jp>
2018-01-16 23:08:49 +0900

    edit 07-01

--- jQuery/07-01/main.js
+++ jQuery/07-01/main.js
@@ -1,3 +1,32 @@
-(() => {
+var resetBtn = document.getElementById('reset');
+var mapZone = document.getElementById('map');
 
-})();
+function initMap() {
+  var map = new google.maps.Map(mapZone, {
+    zoom: 15,
+    center: {lat: 35.685176, lng: 139.710052}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+      resetBtn.addEventListener('click', function(){
+        initMap(results[0].geometry.location);
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
