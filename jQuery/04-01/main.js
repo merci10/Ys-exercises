9be4df7d0848eba53b707534c3271d56eb75a29a
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-24 15:34:47 +0900

    edit 04-01

--- jQuery/04-01/main.js
+++ jQuery/04-01/main.js
@@ -2,34 +2,27 @@
 
   // ボタンをクリックした雰囲気を表現
   $('.openBtn').mousedown(function(){
-    $(this).addClass('is-active');
+    $(this).addClass('is_active');
   }).mouseup(function(){
-    $(this).removeClass('is-active');
+    $(this).removeClass('is_active');
   });
-
-
+  // モーダルを開く
   $('.openBtn').on('click', () => {
     $('.gray_bg').addClass('is_visible');
     $('.modal_window').addClass('is_visible');
   });
-
-  $('.closeBtn').on('click', () => {
+  // モーダルを閉じる
+  $('.closeBtn, .gray_bg').on('click', () => {
     $('.gray_bg').removeClass('is_visible');
     $('.modal_window').removeClass('is_visible');
   });
 
-  $('.gray_bg').on('click', () => {
-    $('.gray_bg').removeClass('is_visible');
-    $('.modal_window').removeClass('is_visible');
-  });
-
-
-  // modal_mainのカラーチェンジ
   $('.colorNav_item').each((index, element) => {
     const $target = $(element);
     const colorCode = $target.data('color');
-
+    // colorNav_itemの初期設定
     $target.css('background-color', `#${colorCode}`);
+    // colorNav_itemをクリックした時のイベントを設定
     $target.on('click', () => {
       $('.colorBox').css('background-color', `#${colorCode}`);
     });
