abb469691208ada11894c323081c3621148eea8a
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-24 15:51:36 +0900

    tidy up 05

--- jQuery/05-01/main.js
+++ jQuery/05-01/main.js
@@ -6,21 +6,14 @@
       $.ajax({
         type: 'get',
         dataType: 'xml',
-        // data: {
-        //   postal: zipcode_str
-        // },
         crossDomain: true,
         url: `http://153.126.194.210/ajax.php?url=http%3A%2F%2Fgeoapi.heartrails.com%2Fapi%2Fxml%3Fmethod%3DsearchByPostal%26postal%3D${zipcode_str}`,
         success: (xml) => {
-          // console.log(xml.responseXML)
           $(xml).find('response').each((i, element) => {
             const el = $(element).text();
             console.log(el);
             $('.distArea').append(el);
           });
-          // $(data).children().each((index, element) => {
-          //   $('.distArea').append($(element));
-          // });
         }
       });
     } else {
--- jQuery/05-02/main.js
+++ jQuery/05-02/main.js
@@ -6,9 +6,6 @@
       $.ajax({
         type: 'get',
         dataType: 'json',
-        // data: {
-        //   postal: zipcode_str
-        // },
         crossDomain: true,
         url: `http://153.126.194.210/ajax.php?url=http%3A%2F%2Fgeoapi.heartrails.com%2Fapi%2Fjson%3Fmethod%3DsearchByPostal%26postal%3D${zipcode_str}`,
         success: (data) => {
