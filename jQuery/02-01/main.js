80d61e902d4113299881e4cf9fcf35f6a35ef25e
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-24 15:19:39 +0900

    edit 02-01

--- jQuery/02-01/main.js
+++ jQuery/02-01/main.js
@@ -6,6 +6,7 @@
     let minutes      = newDate.getMinutes();
     let seconds      = newDate.getSeconds();
 
+    // 数字が一桁の時に前に0をつける
     hours   = hours   < 10 ? `0${hours}`   : hours;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
