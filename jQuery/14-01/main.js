f250bc4e8f5a984a48bd0d59cd2ba4f23d74b07e
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-30 12:01:31 +0900

    edit 14-01

--- jQuery/14-01/main.js
+++ jQuery/14-01/main.js
@@ -1,13 +1,22 @@
 (() => {
   // モーダルの開閉
   // -------------------------------------------------------------
-  $('.openBtn').on('click', () => {
+  const openModal = () => {
     $('.gray_bg').addClass('is_visible');
     $('.modal_window').addClass('is_visible');
-  });
-  $('.gray_bg').on('click', () => {
+  }
+
+  const closeModal = () => {
     $('.gray_bg').removeClass('is_visible');
     $('.modal_window').removeClass('is_visible');
+  }
+
+  $('.openBtn').on('click', () => {
+    openModal();
+  });
+
+  $('.gray_bg').on('click', () => {
+    closeModal();
   });
 
 
@@ -15,6 +24,8 @@
   // ------------------------------------------------------------
   // 曜日の配列
   const days = ["日","月","火","水","木","金","土"];
+  // 選択された日付を表示するinput
+  const $dateInput = $('.dateInput');
   // 月替えボタン
   const $prevBtn = $('.prevBtn');
   const $nextBtn = $('.nextBtn');
@@ -24,10 +35,15 @@
   // table関連
   const $thead = $('.calendar_thead');
   const $tbody = $('.calendar_tbody');
-  const today = new Date();
-  const currentYear = today.getFullYear();
-  const currentMonth = today.getMonth();
+  const now = new Date();
+  // 表示されているカレンダーの情報(この数値を変えていく)
+  let currentYear = now.getFullYear();
+  let currentMonth = now.getMonth();
+  let currentDate = now.getDate();
 
+
+  // カレンダーを作るための関数
+  // =================================================================
   const insertCalendarTitle = (year, month) => {
     $year.text(year);
     $month.text(month + 1);
@@ -47,13 +63,14 @@
 
   // 日にちの部分を作成する
   const createCalandarTbody = (year, month, today) => {
+    const isNowYM = now.getFullYear() === year && now.getMonth() === month ? true : false;
     const startDate = new Date(year, month, 1); // 今の月の最初の日
     const endDate = new Date(year, month + 1, 0); // 今の月の最後の日
     // [注意]startは曜日を取得しているが、endは日にち
     const startDay = startDate.getDay(); // 今の月の最初の日の曜日
     const endDay = endDate.getDate(); // 今の月の最後の日にち
-    let textSkip = true; // 日にちを埋めるようのフラグ
-    let textDate = 1; // 日付(カウントアップしていく)
+    let isTextSkip = true; // 日にちを埋めるようのフラグ
+    let numDate = 1; // 日付(カウントアップしていく)
 
     let tableBody = ''; // tbodyの中身になるもの
     for (let row = 0; row < 6; row++) {
@@ -61,19 +78,19 @@
 
       for (let col = 0; col < 7; col++) {
         if (row === 0 && startDay === col) {
-          textSkip = false;
+          isTextSkip = false;
         }
-        if (textDate > endDay) {
-          textSkip = true;
+        if (numDate > endDay) {
+          isTextSkip = true;
         }
 
-        let isToday = textDate === today.getDate();
-        let className = isToday ? 'is_today' : '';
+        let isToday = (numDate === today);
+        let className = isNowYM && isToday ? 'is_today' : '';
         let td;
-        if (textSkip === false) {
-          td = `<td class="calendar_elem calendar_day ${className}">${textDate}</td>`
-          textDate++
-        } else if (textSkip === true) {
+        if (isTextSkip === false) {
+          td = `<td class="calendar_elem calendar_day ${className}">${numDate}</td>`
+          numDate++
+        } else if (isTextSkip === true) {
           td = `<td class="calendar_elem"></td>`;
         }
         tr += td;
@@ -83,12 +100,49 @@
     }
     $tbody.html(tableBody);
   }
-
+  
+  // カレンダーの初期設定
   const setUp = () => {
-    createCalandarThead();
-    createCalandarTbody(currentYear, currentMonth, today);
     insertCalendarTitle(currentYear, currentMonth);
+    createCalandarThead();
+    createCalandarTbody(currentYear, currentMonth, currentDate);
   }
 
+
+  // prevBtnを押した時のアクション
+  // ---------------------------------------------------------------
+  $prevBtn.on('click', () => {
+    const prev = new Date(currentYear, currentMonth - 1);
+    currentYear = prev.getFullYear();
+    currentMonth = prev.getMonth();
+    insertCalendarTitle(currentYear, currentMonth);
+    createCalandarTbody(currentYear, currentMonth, currentDate);
+  });
+
+
+  // nextBtnを押した時のアクション
+  // ---------------------------------------------------------------
+  $nextBtn.on('click', () => {
+    const next = new Date(currentYear, currentMonth + 1);
+    currentYear = next.getFullYear();
+    currentMonth = next.getMonth();
+    insertCalendarTitle(currentYear, currentMonth);
+    createCalandarTbody(currentYear, currentMonth, currentDate);
+  });
+
+
+  // カレンダーの日にちをクリックした時のアクション
+  // --------------------------------------------------------------
+  $tbody.on('click', '.calendar_day', (el) => {
+    const $target = $(el.target);
+    const year = currentYear;
+    const month = currentMonth;
+    const day = $target.text();
+    $dateInput.val(`${year}/${month + 1}/${day}`)
+    closeModal();
+  });
+
+
+  // 画面ロード時の処理
   setUp();
 })();
