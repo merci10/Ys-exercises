94de3def985c26674cb344f7d248dd297e3339e0
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-01-31 16:36:13 +0900

    edit 08-01

--- jQuery/08-01/main.js
+++ jQuery/08-01/main.js
@@ -4,11 +4,22 @@
   const $dropArea = $('.dropArea');
   const $distArea = $('.distArea');
 
-  $distArea.on('drop', (el) => {
-    const $target = el.originalEvent;
-    $target.stopPropagation();
-    $target.preventDefault();
 
-    console.log($target);
+  // drag&dropの処理
+  $dropArea.on('dragover', function(e){
+    e.preventDefault();
+    $(this).addClass('is_active');
+    return false;
+  }).on('dragleave', function(e){
+    e.preventDefault();
+    $(this).removeClass('is_active');
+    return false;
+  }).on('drop', function(e){
+    e.preventDefault();
+    // dropしたファイル名を取得
+    const fileName = e.originalEvent.dataTransfer.files[0].name;
+    $distArea.text(fileName);
+    $(this).removeClass('is_active');
+    return false;
   });
 })();
