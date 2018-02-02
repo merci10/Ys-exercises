37aee59e11ace82030bfacafb969cdab7acdd13a
川口 侑真 <yuma_kawaguchi@waku-2.com>
2018-02-02 12:04:46 +0900

    edit 07-03

--- jQuery/07-03/main.js
+++ jQuery/07-03/main.js
@@ -1,30 +1,55 @@
 'use strict';
 var map;
 var marker;
+var infowindow;
 
 function initMap() {
   var firstLocation = {lat: 35.685176, lng: 139.710052};
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 16,
     center: firstLocation
   });
-  marker = new google.maps.Marker({
-    position: firstLocation,
-    map: map,
-    title: 'Hello World'
-  });
+  var geocoder = new google.maps.Geocoder();
+
+
   map.addListener('click', function(e){
-    console.log(e);
-    addMarker(e.latLng);
+    // クリックした箇所の緯度経度を取得
+    var location = e.latLng;
+    // マーカーが存在していれば消す
+    if (marker) clearMarker();
+    addMarker(location);
+    // geocoderでクリックした箇所の詳しい住所情報を取得する
+    geocoder.geocode({'location': location}, function(results, status) {
+      if (status === 'OK') {
+        openInfowindow(results[0]);
+      }
+    })
   })
 }
 
+function openInfowindow(results) {
+  var address = results.formatted_address;
+  var lat = results.geometry.location.lat();
+  var lng = results.geometry.location.lng();
+  var contentString = '<dl>' +
+    '<dt>■住所</dt>' +
+    '<dd>' + address + '</dd>' +
+    '<dt>■緯度・経度</dt>' +
+    '<dd>' + lat + ', ' + lng + '</dd>' +
+    '</dl>';
+  infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  infowindow.open(map, marker);
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
 function addMarker(location) {
-  clearMarker();
   marker = new google.maps.Marker({
     position: location,
     map: map,
-    title: 'Hello World'
   });
 }
 
